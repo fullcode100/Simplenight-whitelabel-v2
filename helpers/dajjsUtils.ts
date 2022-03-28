ec9b6fe9e4f6cd4101c27546a77061d18707d63f
Juan <juan.rodriguezsotto@gmail.com>
2022-03-28 09:33:14 -0300

    Modify search date format constant location to avoid cyclic dependancy

--- helpers/dajjsUtils.ts
+++ helpers/dajjsUtils.ts
@@ -2,5 +2,7 @@ import dayjs from 'dayjs';
 
 const MONTH_DAY_FORMAT = 'M/D';
 
+export const SEARCH_DATE_FORMAT = 'YYYY-MM-DD';
+
 export const formatAsMonthDay = (date: string | number | Date | dayjs.Dayjs) =>
   dayjs(date).format(MONTH_DAY_FORMAT);
--- providers/categoryProvider.tsx
+++ providers/categoryProvider.tsx
@@ -2,6 +2,5 @@ import { HotelCategory } from 'hotels';
 
 import { CategoryOptions } from '../types/search/SearchTypeOptions';
 
-export const SEARCH_DATE_FORMAT = 'YYYY-MM-DD';
 
 export const categoryOptions: CategoryOptions = [HotelCategory];
