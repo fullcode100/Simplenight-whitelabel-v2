5c453dfaaafe86cf2c38fa8f0ad35c2339555c5f
Juan <juan.rodriguezsotto@gmail.com>
2022-04-04 12:23:31 -0300

    Add scroll to header

--- hotels/components/search/HotelSearchForm.tsx
+++ hotels/components/search/HotelSearchForm.tsx
@@ -20,6 +20,11 @@ import NumberInput from 'components/global/Input/NumberInput';
 import Button from 'components/global/Button/Button';
 import { SearchFormProps } from 'types/search/SearchFormProps';
 import useQuerySetter from 'hooks/pageInteraction/useQuerySetter';
+import LocationInput from 'components/global/Input/LocationInput';
+import useQuery from 'hooks/pageInteraction/useQuery';
+import { formatAsSearchDate } from 'helpers/dajjsUtils';
+import { parseQueryNumber } from 'helpers/stringUtils';
+import { StringGeolocation } from 'types/search/Geolocation';
 
 const SEARCH_DATE_FORMAT = 'YYYY-MM-DD';
 
@@ -42,90 +47,68 @@ const getInitialHotelOccupancyState = (): OccupancyData => ({
 });
 
 const HotelSearchForm = ({ setIsReading }: SearchFormProps) => {
-  const [searchData, setSearchData] = useState(getInitialHotelSearchState);
-  const [occupancyData, setOccupancyData] = useState<OccupancyData>(
-    getInitialHotelOccupancyState,
+  const setQueryParam = useQuerySetter(['type']);
+
+  const [adultCount, setAdultCount] = useState<Number>(0);
+  const [geolocation, setGeolocation] = useState<StringGeolocation>();
+  const [childrenCount, setChildrenCount] = useState<Number>(0);
+  const [roomCount, setRoomCount] = useState<Number>(0);
+  const [startDate, setStartDate] = useState<string>(
+    formatAsSearchDate(dayjs()),
   );
-  const setQueryParam = useQuerySetter();
-  const [isOccupancySelectorVisible, setIsOccupancySelectorVisible] =
-    useState(false);
-
-  const [guestCount, setGuestCount] = useState(
-    occupancyData.adultCount + occupancyData.childCount,
-  );
-
-  const roomLabel = usePlural(occupancyData.roomCount, 'Room', 'Rooms');
-  const guestLabel = usePlural(guestCount, 'Guest', 'Guests');
-
-  const handleLocationSelect = (location: LocationPrefix) => {
-    setSearchData({
-      ...searchData,
-      startLocation: location,
-    });
-  };
-
-  const handleDateChange = (date: string) => {
-    const formattedDate = dayjs(date).format(SEARCH_DATE_FORMAT);
-
-    setSearchData({
-      ...searchData,
-      startDate: formattedDate,
-    });
-  };
-
-  const handleClickOccupancySelector = () => {
-    setIsOccupancySelectorVisible(true);
-  };
-
-  const handleOccupancySelectorClose = (selectedOccupancy: OccupancyData) => {
-    setOccupancyData(selectedOccupancy);
-    setIsOccupancySelectorVisible(false);
-  };
-
-  useEffect(() => {
-    setGuestCount(occupancyData.adultCount + occupancyData.childCount);
-  }, [occupancyData]);
-
-  const StartLocationPicker = () => (
-    <LocationAutoComplete
-      className="block w-3/5 mr-10 placeholder-primary text-primary"
-      onSelect={handleLocationSelect}
-      placeholder="Search for a location"
-    />
+  const [endDate, setEndDate] = useState<string>(
+    formatAsSearchDate(dayjs().add(1, 'day')),
   );
 
-  const StartDatePicker = () => (
-    <DatePicker
-      className="w-1/8 h-full border-gray-600 border-2"
-      value={searchData.startDate}
-      onChange={(e: ChangeEvent<HTMLInputElement>) =>
-        handleDateChange(e.target.value)
-      }
-    />
-  );
+  const handleRoomChange = (value: Number) => {
+    setRoomCount(value);
+  };
+
+  const handleAdultChange = (value: Number) => {
+    setAdultCount(value);
+  };
+
+  const handleChildrenCount = (value: Number) => {
+    setChildrenCount(value);
+  };
+
+  const handleStartDateChange = (value: string) => {
+    setStartDate(value);
+  };
+
+  const handleEndDateChange = (value: string) => {
+    setEndDate(value);
+  };
 
   const handleSearchClick = async () => {
-    await setQueryParam('adults', '4');
-    await setQueryParam('children', '2');
-    await setQueryParam('startDate', '2020-01-01');
-    await setQueryParam('endDate', '2020-01-02');
+    await setQueryParam('adults', adultCount + '');
+    await setQueryParam('children', childrenCount + '');
+    await setQueryParam('startDate', startDate);
+    await setQueryParam('endDate', endDate);
+    await setQueryParam('geolocation', geolocation ?? '');
     if (setIsReading) setIsReading(true);
   };
 
   return (
     <section className="">
-      <IconInput
+      <LocationInput
         icon={<LocationPin className="h-5 w-5 text-dark-700" />}
         label=""
-        name="rooms"
+        name="location"
         placeholder="Chicago, IL, USA"
+        routeParams={['type']}
+        onSelect={(value: StringGeolocation) => setGeolocation(value)}
       />
 
       <NumberInput
         label="Rooms"
         name="rooms"
         placeholder="1 Room"
-        className="mt-4"
+        className="h-full items-center"
+        value={roomCount}
+        onChange={(event) =>
+          handleRoomChange(parseQueryNumber(event.target.value))
+        }
       />
 
       <IconInput
@@ -134,7 +117,10 @@ const HotelSearchForm = ({ setIsReading }: SearchFormProps) => {
         placeholder="2 adults"
         icon={<MultiplePersons className="h-5 w-5 text-dark-700" />}
         className="mt-4"
-        orientation="right"
+        // value={adultCount}
+        onChange={(event) =>
+          handleAdultChange(parseQueryNumber(event.target.value))
+        }
       />
 
       <section className="flex gap-4">
@@ -145,6 +131,8 @@ const HotelSearchForm = ({ setIsReading }: SearchFormProps) => {
           className="mt-4"
           orientation="right"
           icon={<Calendar className="h-5 w-5 text-dark-700" />}
+          value={startDate}
+          onChange={(event) => handleStartDateChange(event.target.value)}
         />
         <IconInput
           label="Check-out"
@@ -153,6 +141,8 @@ const HotelSearchForm = ({ setIsReading }: SearchFormProps) => {
           orientation="right"
           className="mt-4"
           icon={<Calendar className="h-5 w-5 text-dark-700" />}
+          value={endDate}
+          onChange={(event) => handleEndDateChange(event.target.value)}
         />
       </section>
 
