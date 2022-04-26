142aaba8d0ea3ad01b72bf23f79349995ff247b3
rodriguez-gaston-simplenight <102606879+rodriguez-gaston-simplenight@users.noreply.github.com>
2022-04-26 14:24:24 -0300

    New room cards with price breakdown (#37)
    * New room cards with price breakdown
    
    * Feature/saf 349/mockup filters search page (#38)
    
    * Add filter button and basic modal
    
    * Add filters form mock
    
    * Fix rebase conflicts
    
    * Add sort by select (#39)
    
    * Fix on PR
    
    * Fix build errors
    
    * Update search form to match the design (#40)
    
    Co-authored-by: Juan Pablo Rodriguez Sotto <juan.rodriguezsotto@gmail.com>

--- components/global/ReadMore/SeeMore.tsx
+++ components/global/ReadMore/SeeMore.tsx
@@ -3,7 +3,9 @@ import classnames from 'classnames';
 import SeeMoreButton from './components/SeeMoreButton';
 
 interface SeeMoreProps {
+  type: 'text' | 'component';
   className?: string;
+  text?: string;
   children?: ReactNode;
   heightInPixels?: number;
   textOpened: string;
@@ -14,7 +16,9 @@ interface SeeMoreProps {
 const SHOW_LESS_HEIGHT = 232;
 
 const SeeMore = ({
+  type,
   className = '',
+  text,
   children,
   heightInPixels = SHOW_LESS_HEIGHT,
   textOpened,
@@ -22,6 +26,8 @@ const SeeMore = ({
   displayButton,
 }: SeeMoreProps) => {
   const [isOpen, setIsOpen] = useState(false);
+
+  const [formattedText, setFormattedText] = useState(text);
 
   const [sectionHeight, setSectionHeight] = useState<string>(
     `${heightInPixels}px`,
@@ -36,6 +42,9 @@ const SeeMore = ({
       setSectionHeight(`${heightInPixels}px`);
       return;
     }
+
+    const newText = text.substring(0, charCount);
+    setFormattedText(newText + '...');
   }, [isOpen]);
 
   useEffect(() => {
@@ -45,6 +54,22 @@ const SeeMore = ({
   }, []);
 
   const toggle = () => setIsOpen(!isOpen);
+
+  if (type === 'text') {
+    return (
+      <section className={`${className} relative `}>
+        <section className={classnames('pb-4', className)}>
+          {formattedText}
+        </section>
+        <SeeMoreButton
+          onClick={toggle}
+          isOpen={isOpen}
+          textOpened={textOpened}
+          textClosed={textClosed}
+        />
+      </section>
+    );
+  }
 
   return (
     <section className={`${className} relative`}>
