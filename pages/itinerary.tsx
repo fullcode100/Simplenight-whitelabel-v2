03ec4d5f5ad6c24664e09b0ca99424df1f597c97
daiana-simplenight <94717001+daiana-simplenight@users.noreply.github.com>
2022-05-05 11:11:37 -0300

    Divide itinerary page in sections (#61)

--- pages/itinerary.tsx
+++ pages/itinerary.tsx
@@ -1,7 +1,14 @@
 import type { NextPage } from 'next';
 
 const Itinerary: NextPage = () => {
-  return <></>;
+  return (
+    <main>
+      <header></header>
+      <section></section>
+      <section></section>
+      <aside></aside>
+    </main>
+  );
 };
 
 export default Itinerary;
