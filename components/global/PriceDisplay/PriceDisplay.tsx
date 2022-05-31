97a68168eb5676d1ed0cee0e382f66d480c13c41
Juan <juan.rodriguezsotto@gmail.com>
2022-05-31 15:54:07 -0300

    Make discounts optional

--- components/global/PriceDisplay/PriceDisplay.tsx
+++ components/global/PriceDisplay/PriceDisplay.tsx
@@ -8,16 +8,20 @@ interface PriceDisplayProps {
 const PriceDisplay = ({ rate }: PriceDisplayProps) => {
   const { total_amount: totalAmount, rate_breakdown: rateBreakdown } = rate;
   const { discounts } = rateBreakdown;
-  const { total_amount_before_apply: totalBeforeDiscount } = discounts;
+  let totalBeforeDiscount;
+  if (discounts)
+    ({ total_amount_before_apply: totalBeforeDiscount } = discounts);
 
   return (
     <section>
-      <p className="text-primary-1000 text-xs">
-        <span className="text-dark-800 line-through">
-          {totalBeforeDiscount.formatted}
-        </span>{' '}
-        25% Off
-      </p>
+      {totalBeforeDiscount && (
+        <p className="text-primary-1000 text-xs">
+          <span className="text-dark-800 line-through">
+            {totalBeforeDiscount.formatted}
+          </span>{' '}
+          25% Off
+        </p>
+      )}
       <p className="text-sm text-dark-1000 font-semibold">
         {totalAmount.formatted}
       </p>
