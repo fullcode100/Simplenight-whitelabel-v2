17bcf25d80d69847757817b5a807c73fd325d3ff
enriquesimplenight <103146440+enriquesimplenight@users.noreply.github.com>
2022-06-01 12:11:19 -0400

    feat: show the drop-down if there is more than one category (#109)

--- layouts/header/components/Menu/Menu.tsx
+++ layouts/header/components/Menu/Menu.tsx
@@ -3,13 +3,22 @@ import LanguageSelect from 'components/global/LanguageSelect/LanguageSelect';
 import TitleDrop from 'components/global/TitleDrop/TitleDrop';
 import CurrencySelect from 'components/global/CurrencySelect/CurrencySelect';
 import Divider from 'components/global/Divider/Divider';
+import { getFeatures } from 'store/selectors/core';
+import { useSelector } from 'react-redux';
 
 const Menu = () => {
+  const features = useSelector(getFeatures);
+  const activeFeatures = Object.keys(features).filter(
+    (feature) => features[feature],
+  );
+  const displayDropdown = activeFeatures.length > 1;
   return (
     <section className="p-4">
-      <section className="mb-2">
-        <CategorySelect />
-      </section>
+      {displayDropdown && (
+        <section className="mb-2">
+          <CategorySelect />
+        </section>
+      )}
       <TitleDrop title="Language">
         <LanguageSelect />
       </TitleDrop>
