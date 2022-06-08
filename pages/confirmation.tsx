d0447dc17bb28a91eef3b3576e3fe77057ed45b5
Ariana-Amador <94371542+Ariana-Amador@users.noreply.github.com>
2022-06-08 16:03:18 -0400

    Added payment methods section (#156)

--- pages/confirmation.tsx
+++ pages/confirmation.tsx
@@ -5,6 +5,7 @@ import ConfirmationHeader from 'components/confirmation/ConfirmationHeader/Confi
 import ConfirmationItemList from 'components/confirmation/ConfirmationItemList/ConfirmationItemList';
 import { formatBooking } from 'helpers/bookingUtils';
 import { bookingMock } from 'mocks/bookingMock';
+import ConfirmationPayment from 'components/confirmation/ConfirmationPayment/ConfirmationPayment';
 
 const Confirmation: NextPage = () => {
   const booking = formatBooking(bookingMock.booking);
@@ -17,6 +18,7 @@ const Confirmation: NextPage = () => {
       <section className="p-5">
         <ConfirmationItemList booking={booking} />
       </section>
+      <ConfirmationPayment booking={booking} />
       <ConfirmationFooter booking={booking} />
     </main>
   );
