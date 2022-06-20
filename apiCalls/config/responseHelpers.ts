6ae7fb9866eaa346b7245cfe85c64d4d426bfb12
rodriguez-gaston-simplenight <102606879+rodriguez-gaston-simplenight@users.noreply.github.com>
2022-06-20 17:55:08 -0300

    Remove duplicated slash on endpoint (#211)

--- apiCalls/config/responseHelpers.ts
+++ apiCalls/config/responseHelpers.ts
@@ -20,7 +20,7 @@ export const applyApiBaseUrlV2 = (
   // const host = request.headers.host;
   // if (host && host.includes('qa')) return `${API_QA_V2_URL}${endpoint}`;
   // return `${API_V2_URL}${endpoint}`;
-  return `${API_PROD_V2_URL}/${endpoint}`;
+  return `${API_PROD_V2_URL}${endpoint}`;
 };
 
 export const forwardError = (err: any, res: any) => {
