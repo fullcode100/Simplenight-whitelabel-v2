001676d5e1119de850fbb60756a3d331751d1983
rodriguez-gaston-simplenight <102606879+rodriguez-gaston-simplenight@users.noreply.github.com>
2022-07-05 12:51:01 -0300

    Fix continue shopping to use the localStorage item (#231)

--- hooks/localStorage/useLocalStorage.tsx
+++ hooks/localStorage/useLocalStorage.tsx
@@ -11,7 +11,7 @@ const useLocalStorage = (key: string, initialValue: string) => {
       // Get from local storage by key
       const item = window.localStorage.getItem(key);
       // Parse stored json or if none return initialValue
-      return item ? JSON.parse(item) : initialValue;
+      return item ? item : initialValue;
     } catch (error) {
       return initialValue;
     }
