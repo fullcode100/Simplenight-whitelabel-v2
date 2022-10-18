5874e5125e1e880e2d34252190159b471ead6235
enriquesimplenight <103146440+enriquesimplenight@users.noreply.github.com>
2022-10-18 12:08:29 -0400

    feat: hotel itinerary disclaimer rates change mock (#404)

--- components/global/CollapseBordered/CollapseBordered.tsx
+++ components/global/CollapseBordered/CollapseBordered.tsx
@@ -6,16 +6,23 @@ import CollapseBody from './components/CollapseBody';
 import CollapseFooter from './components/CollapseFooter';
 
 interface CollapseBorderedProps {
+  disclaimer?: ReactNode;
   title: ReactNode;
   body: ReactNode;
   footer: ReactNode;
 }
 
-const CollapseBordered = ({ title, body, footer }: CollapseBorderedProps) => {
+const CollapseBordered = ({
+  disclaimer,
+  title,
+  body,
+  footer,
+}: CollapseBorderedProps) => {
   const [show, setShow] = useState(false);
 
   return (
     <section className="border border-dark-300 rounded">
+      {disclaimer}
       <CollapseHeader title={title} show={show} setShow={setShow} />
       <Collapse isOpened={show}>
         <CollapseBody body={body} show={show} />
