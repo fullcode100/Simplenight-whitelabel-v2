65d10950d0e0135a2dd10c20e6a032bc08db3103
rodriguez-gaston-simplenight <102606879+rodriguez-gaston-simplenight@users.noreply.github.com>
2022-10-20 11:09:25 -0300

    Fix prices and show discount in specific urls (#415)

--- hotels/components/PriceDisplay/PriceDisplay.tsx
+++ hotels/components/PriceDisplay/PriceDisplay.tsx
@@ -1,9 +1,11 @@
+/* eslint-disable indent */
 import React from 'react';
 import classnames from 'classnames';
 
 import { Rates } from '../../types/response/SearchResponse';
 import { useTranslation } from 'react-i18next';
 import TaxesAndFeesPopover from 'hotels/components/TaxesAndFeesPopover/TaxesAndFeesPopover';
+import { useRouter } from 'next/router';
 
 interface PriceDisplayProps {
   rate: Rates;
@@ -18,6 +20,8 @@ const PriceDisplay = ({
   isSearch = false,
   isPriceBase,
 }: PriceDisplayProps) => {
+  const router = useRouter();
+  const pathName = router.pathname;
   const totalAmount = rate?.min_rate.rate.total_amount.formatted;
   const avgAmount = rate?.avg_amount?.avg_amount?.formatted;
   const startingRoomTotal = rate?.min_rate?.rate.starting_room_total;
@@ -29,38 +33,38 @@ const PriceDisplay = ({
     'Includes Taxes and Fees',
   );
 
-  let totalBeforeDiscount;
+  const totalBeforeDiscount =
+    rate?.min_rate.rate?.rate_breakdown.discounts?.total_amount_before_apply;
   let percentageToApply;
   if (discounts) {
-    ({
-      total_amount_before_apply: totalBeforeDiscount,
-      percentage_to_apply: percentageToApply,
-    } = discounts);
+    ({ percentage_to_apply: percentageToApply } = discounts);
   }
+  const showDiscount =
+    pathName.startsWith('/checkout') || pathName.startsWith('/confirmation')
+      ? false
+      : true;
 
-  // const baseBeforeApply = rate?.avg_amount?.discounts?.base_amount_before_apply;
-  isPriceBase &&
-    (totalBeforeDiscount =
-      rate?.min_rate.rate?.rate_breakdown.discounts?.total_amount_before_apply);
   return (
     <section className="text-right">
-      {totalBeforeDiscount && (
-        <p className="text-xs">
-          <span className="text-dark-700 line-through font-normal">
-            {totalBeforeDiscount.formatted}
-          </span>{' '}
-          <span className="text-green-1000 font-semibold">
-            {percentageToApply} Off
-          </span>
-        </p>
-      )}
+      {showDiscount &&
+        totalBeforeDiscount.amount >
+          rate?.min_rate.rate.total_amount.amount && (
+          <p className="text-xs">
+            <span className="text-dark-700 line-through font-normal">
+              {totalBeforeDiscount.formatted}
+            </span>{' '}
+            <span className="text-green-1000 font-semibold">
+              {percentageToApply} Off
+            </span>
+          </p>
+        )}
       <p
         className={classnames('leading-[22px] text-dark-1000', {
           ['flex flex-row gap-1 justify-end']: totalLabel,
         })}
       >
         <span className="text-xs">{totalLabel}</span>
-        <span className="text-sm font-semibold">{avgAmount}</span>
+        <span className="text-sm font-semibold">{totalAmount}</span>
       </p>
       {isSearch && startingRoomTotal && (
         <p className="text-[12px] leading-[15px] text-dark-1000 flex flex-row gap-1 justify-end">
