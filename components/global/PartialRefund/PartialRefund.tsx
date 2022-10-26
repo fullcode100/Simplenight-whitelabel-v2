677bf64fcdb26581434bb6dfccb74a45b02177d2
isabel-light-it <97675473+isabel-light-it@users.noreply.github.com>
2022-10-26 16:05:44 -0300

    Feature/saf 2544/activity cancellation policy (#426)
    * cancellation section
    
    * Update cancellation tags styles
    
    * update cancellation tags
    
    * fix: cancellation tags
    
    * Add translations
    
    * update mock
    
    * wip: see more logic
    
    * add see more
    
    * add cancellation conditions

--- components/global/PartialRefund/PartialRefund.tsx
+++ components/global/PartialRefund/PartialRefund.tsx
@@ -19,10 +19,9 @@ const PartialRefund = ({
   if (!nonCancellable) return null;
   return (
     <section
-      className={classnames(
-        'bg-dark-100 min-h-8 w-fit border border-dark-300 rounded',
-        { 'w-full': wfull },
-      )}
+      className={classnames('bg-dark-100 min-h-8 w-fit rounded', {
+        'w-full': wfull,
+      })}
     >
       <section className="flex gap-1.5 px-2 py-1.5">
         <label className="text-dark-1000 text-[14px] leading-[20px] font-semibold">
