9891806bf2ed368d142eb9d70cc57be50a21a8ee
daiana-simplenight <94717001+daiana-simplenight@users.noreply.github.com>
2022-11-29 12:24:11 -0300

    Use curlirize for admin requests (#518)

--- apiCalls/config/axiosHelperSettings.ts
+++ apiCalls/config/axiosHelperSettings.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import curlirize from 'axios-curlirize';
 
 export default (() => {
   const instance = axios.create({
@@ -7,6 +8,15 @@ export default (() => {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${process.env.NEXT_PUBLIC_ADMIN_TOKEN}`,
     },
+  });
+
+  curlirize(instance, (result: any, err: any) => {
+    const curl = result.command;
+    if (err) {
+      console.error(`Failed request curl: ${curl}`);
+    } else {
+      console.log(`Successful request curl: ${curl}`);
+    }
   });
 
   return instance;
