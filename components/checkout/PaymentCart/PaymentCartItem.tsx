bdc7c815e34251c408f023236857792d0ddc5814
jarriola-sn <117696303+jarriola-sn@users.noreply.github.com>
2022-12-27 13:53:39 -0600

    showing cart details on payment page (#747)
    Co-authored-by: ing-arriola <ingeniero.arriola@gmail.com>

--- components/checkout/PaymentCart/PaymentCartItem.tsx
+++ components/checkout/PaymentCart/PaymentCartItem.tsx
@@ -1,50 +1,8 @@
 /* eslint-disable camelcase */
 import { useCategory } from 'hooks/categoryInjection/useCategory';
 import { injectProps } from 'helpers/reactUtils';
-import CollapseBordered from 'components/global/CollapseBordered/CollapseBordered';
-import BreakdownSummary from 'hotels/components/PriceBreakdownModal/components/BreakdownSummary';
-import { Item } from 'types/cart/CartType';
-import { useCategorySlug } from 'hooks/category/useCategory';
-
-interface PaymentCartItemProps {
-  item: Item;
-}
 
 const PaymentCartItem = ({ item, customer }: any) => {
-  const categoryLowerCase = item.category
-    ? item.category.toLowerCase()
-    : 'no category';
-  const CartItemHeader = () => {
-    const category = useCategory(
-      item.category.toLowerCase() === 'car-rental'
-        ? 'cars'
-        : item.category.toLowerCase(),
-    );
-    return injectProps(category?.checkoutDisplay, {
-      item: item,
-    });
-  };
-
-  const CartItemBreakdown = () => {
-    const category = useCategory(
-      item.category.toLowerCase() === 'car-rental'
-        ? 'cars'
-        : item.category.toLowerCase(),
-    );
-    return injectProps(category?.breakdownDisplay, {
-      item: item,
-      showCollapse: false,
-    });
-  };
-
-  const CartItemBody = () => {
-    return (
-      <section className="px-4 mb-6">
-        <CartItemBreakdown />
-      </section>
-    );
-  };
-
   const CartItemDetail = () => {
     let sectorName = item.sector?.toLowerCase();
     const categoryName = item.category?.toLowerCase();
@@ -60,6 +18,9 @@ const PaymentCartItem = ({ item, customer }: any) => {
       sectorName = 'car-rental';
     if (sectorName === 'food-beverage') {
       sectorName = 'dining';
+    }
+    if (sectorName === 'accommodations') {
+      sectorName = 'hotels';
     }
     const sector = useCategory(sectorName || '');
     return injectProps(sector?.checkoutItemDisplay, {
