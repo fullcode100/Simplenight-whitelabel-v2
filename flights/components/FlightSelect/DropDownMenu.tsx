62627c7a9d7707a5ca814f8f3909160ac1ad9abb
rodriguez-gaston-simplenight <102606879+rodriguez-gaston-simplenight@users.noreply.github.com>
2023-05-09 09:41:22 -0300

    Fix: Class Selector doesn't have Selected Status (#1016)
    Co-authored-by: vladimirsimplenight <117688911+vladimirsimplenight@users.noreply.github.com>

--- flights/components/FlightSelect/DropDownMenu.tsx
+++ flights/components/FlightSelect/DropDownMenu.tsx
@@ -46,7 +46,7 @@ const DropdownMenu = ({
   };
 
   return (
-    <section className=" tems-center justify-start relative">
+    <section className="relative justify-start tems-center">
       <button
         className="flex items-center whitespace-nowrap bg-white rounded border border-gray-300 focus:border-teal-1000 w-full h-8 p-[8px] text-sm text-dark-1000"
         onClick={() => setShowMenu((p) => !p)}
@@ -66,15 +66,19 @@ const DropdownMenu = ({
         {itemsState.map(({ label, icon, isActive, value }, index) => (
           <button
             className={classnames(
-              'flex items-center bg-white border-b hover:bg-teal-100 hover:text-teal-1000 hover:fill-teal-1000 border-gray-300 w-full h-11 py-2 px-[13px] text-sm text-dark-1000',
+              'flex items-center border-b border-gray-300 w-full h-11 py-2 px-[13px] text-sm',
               {
                 'bg-primary-400 text-primary-1000': isActive,
+              },
+              {
+                'bg-white text-dark-1000 hover:bg-teal-100 hover:text-teal-1000 hover:fill-teal-1000':
+                  !isActive,
               },
             )}
             onClick={() => updateItemsState(value)}
             key={index}
           >
-            <span className="flex gap-1 items-center flex-row text-xs font-semibold text-left">
+            <span className="flex flex-row items-center gap-1 text-xs font-semibold text-left">
               <span>{icon}</span>
               <span>{label}</span>
             </span>
