771398567a25a7d63509bb2aa3605522cf9385c2
Isabel Alvarez <isabel@lightit.io>
2023-05-15 10:55:46 -0300

    add flight cancellation request

---
+++ pages/api/flights/bookings/cancellation/[controlNumber].ts
@@ -0,0 +1,32 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import axios from 'axios';
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>,
+) {
+  try {
+    const { body } = req;
+
+    const { controlNumber } = req.query;
+
+    const { data } = await axios.patch(
+      `https://api-dev.simplenight.com/sn-booking-service/reservation/${controlNumber}`,
+      body,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+    );
+
+    if (!data) throw new Error('Flight cancellation error');
+    res.status(200).json({ ...data });
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      res.status(400).json({ booking: {} });
+    } else {
+      res.status(400).json({ booking: {} });
+    }
+  }
+}
