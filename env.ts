65608cbce4d8e335c6ef0851b2989b135cbf180b
Andrés Nakanishi <105232333+NakaSimpleNight@users.noreply.github.com>
2023-05-24 17:33:52 -0300

    Friendly Deployment (#1038)
    * First commit
    
    * [UPDATE] ENV VARIABLES ERRORS
    
    * [DELETE]
    
    * [DELETE]
    
    * update ci-cd with new vars
    
    * cleanup build and deploy
    
    * cloudbuild.yaml substitution update
    
    * update env vars KOUNT and PURCHASE
    
    * [FIX] yarn lock
    
    ---------
    
    Co-authored-by: Naka <andresnakanishi@gmail.com>
    Co-authored-by: Solito Reyes III <solrey3@solrey3.com>

---
+++ env.ts
@@ -0,0 +1,51 @@
+import { omit } from 'lodash/fp';
+import { ZodError, z } from 'zod';
+
+const stringValidation = z.string().min(1, 'Var is not defined');
+const urlValidation = z.string().url().min(1, 'Var is not defined');
+
+const envSchema = z.object({
+  // GOOGLE KEYS
+  NEXT_PUBLIC_GOOGLE_MAP_KEY: stringValidation,
+  // PURCHASE MODE
+  NEXT_PUBLIC_PURCHASE_MODE: z.enum(['cart', 'single']),
+  // ADMIN
+  NEXT_PUBLIC_ADMIN_ENDPOINT_URL: urlValidation,
+  NEXT_PUBLIC_ADMIN_TOKEN: stringValidation,
+  // ENCRYPT
+  NEXT_PUBLIC_ENCRYPT_KEY: stringValidation,
+  // KOUNT
+  NEXT_PUBLIC_KOUNT_CLIENT: stringValidation,
+  NEXT_PUBLIC_KOUNT_ENVIRONMENT: stringValidation,
+});
+
+type EnvValues = z.infer<typeof envSchema>;
+
+// IDEA: trigger a toast message on dev instead of just a console error
+function logEnvError(errors: ZodError<EnvValues>) {
+  const formattedErrors = omit('_errors', errors.format());
+  console.error('<');
+  console.error('ENVIRONMENT VARIABLES ERRORS:');
+  console.error('----');
+  Object.entries(formattedErrors).forEach(([name, { _errors }]) => {
+    const errMsg = _errors.join(', ');
+    console.error(`"${name}": ${errMsg}`);
+  });
+  console.error('----');
+  console.error('>');
+}
+
+function checkEnv() {
+  try {
+    return envSchema.parse(process.env);
+  } catch (errors) {
+    if (errors instanceof ZodError) logEnvError(errors as ZodError<EnvValues>);
+    // So! We had some validation errors
+    // let's just make sure the shape of the object is what we are expecting
+    return Object.fromEntries(
+      Object.keys(envSchema.shape).map((key) => [key, process.env[key] || '']),
+    ) as EnvValues;
+  }
+}
+
+export const envWL = checkEnv();
--- pages/404.tsx
+++ pages/404.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { NextPageWithLayout } from 'types/layout/pageTypes';
-import Button from 'components/global/ButtonNew/Button';
 import { useTranslation } from 'react-i18next';
 import { useRouter } from 'next/router';
 import { Error404 } from '@simplenight/ui';
@@ -8,7 +7,6 @@ import ErrorPage from 'components/global/ErrorPage/ErrorPage';
 
 const Custom404: NextPageWithLayout = () => {
   const [t] = useTranslation('global');
-  const router = useRouter();
   const pageNotFound = t('pageNotFound', 'Page Not Found');
   const sorryText = t('sorryTheRequestedPage', 'Sorry, The Requested Page');
 
