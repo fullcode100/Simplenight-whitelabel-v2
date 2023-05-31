2471f22e2d36f8f062b780b22dd9eea428c1c7eb
Vladimir Cespedes <vladimir@simplenight.com>
2023-05-31 19:01:00 -0400

    remove logs

--- pages/api/flights/bookings/index.ts
+++ pages/api/flights/bookings/index.ts
@@ -17,7 +17,6 @@ export default async function handler(
     await setSession(req);
     applySimplenightApiKey(req, res);
     const axios = createServerAxiosInstance(req);
-    console.log('req = > ', req.headers);
 
     const { body } = req;
 
@@ -25,9 +24,6 @@ export default async function handler(
       'https://dev-ms.simplenight.com/sn-booking-service/reservation',
       body,
     );
-
-    console.log('data from reservation =>  ', data);
-    console.log('uui getted from db ', data.booking?.bookingId);
 
     if (data.errorMessage?.error) {
       res.status(400).json({ error: data.errorMessage?.error }); // TODO: Send error
@@ -41,7 +37,6 @@ export default async function handler(
           `https://dev-ms.simplenight.com/sn-booking-service/ticket/${controlNumber}`,
         );
 
-        console.log('data 1 => ', data1);
         const { data } = await axios.get(
           `https://dev-ms.simplenight.com/sn-booking-service/reservation/${controlNumber}`,
         );
@@ -50,7 +45,6 @@ export default async function handler(
           booking: data,
         });
       } catch (error) {
-        console.log('eerrororor = > ', error);
         res.status(400).json({ booking: {} });
       }
     }
@@ -58,7 +52,6 @@ export default async function handler(
     if (!data) throw new Error('Flight booking error');
     res.status(200).json({ ...data });
   } catch (error) {
-    console.log('errorr => ', error);
     res.status(400).json({ booking: {} });
   }
 }
