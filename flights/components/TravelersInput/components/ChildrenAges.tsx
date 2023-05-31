96c38f01157b2f9d3fcf7ecd1a29d0176374edb3
Isabel Alvarez <isabel@lightit.io>
2023-05-31 14:41:56 -0300

    WIP: update children input

--- flights/components/TravelersInput/components/ChildrenAges.tsx
+++ flights/components/TravelersInput/components/ChildrenAges.tsx
@@ -4,6 +4,7 @@ import BaseInput from 'components/global/Input/BaseInput';
 import { Traveler } from 'flights/helpers/traveler';
 import { changeArraySize } from 'helpers/arrayUtils';
 import classnames from 'classnames';
+import { useState } from 'react';
 
 interface ChildrenAgesProps {
   traveler: Traveler;
@@ -32,22 +33,52 @@ const ChildrenAges = ({
       ? traveler.childrenAges
       : changeArraySize(traveler.childrenAges, newChildrenAmount);
 
-  const validateAge = (age: number) => {
-    let _age = age;
-    if (_age > 11) _age = 11;
-    if (_age < 2) _age = 2;
-    return _age;
+  const ChildrenAgesInput = ({
+    indexAge,
+    age,
+  }: {
+    indexAge: number;
+    age: number;
+  }) => {
+    const [childAge, setChildAge] = useState<number | ''>(age);
+
+    const validateAge = (age: number) => {
+      let _age = age;
+      if (_age > 11) _age = 11;
+      if (_age < 2) _age = 2;
+      setChildAge(_age);
+
+      handleChildrenAgesChange(_age, indexAge, travelerNumber);
+    };
+    return (
+      <input
+        type="number"
+        className="focus:ring-primary-500 focus:border-primary-500 block w-full h-11 w-11 sm:text-sm border-gray-300 rounded text-center"
+        value={childAge !== 0 ? Number(childAge).toString() : ''}
+        onChange={(e) =>
+          setChildAge(e.target.value !== '' ? Number(e.target.value) : '')
+        }
+        onBlur={(e) => validateAge(Number(e.target.value))}
+      />
+    );
   };
 
-  return (
-    <section className={classnames('flex flex-col gap-2', className)}>
-      <section className="text-dark-800 text-[16px] leading-[16px]">
-        {childrenAgesLabel}
-      </section>
-      <section className="flex flex-wrap gap-3">
-        {traveler.childrenAges.map((age, indexAge) => (
-          <section key={indexAge}>
-            <BaseInput
+  /*  return (
+            <section key={indexAge}>
+              <input
+                type="number"
+                value={childAge}
+                className="focus:ring-primary-500 focus:border-primary-500 block w-full h-11 w-11 sm:text-sm border-gray-300 rounded text-center"
+                onBlur={(e) =>
+                  handleChildrenAgesChange(
+                    validateAge(parseInt(e.target.value)),
+                    indexAge,
+                    travelerNumber,
+                  )
+                }
+                onChange={(e) => setChildAge(Number(e.target.value))}
+              />
+              {/* <BaseInput
               type="number"
               value={validateAge(age)}
               onChange={(e) =>
@@ -59,8 +90,18 @@ const ChildrenAges = ({
               }
               min={2}
               max={11}
-            />
-          </section>
+            /> */
+
+  console.log(traveler.childrenAges);
+
+  return (
+    <section className={classnames('flex flex-col gap-2', className)}>
+      <section className="text-dark-800 text-[16px] leading-[16px]">
+        {childrenAgesLabel}
+      </section>
+      <section className="flex flex-wrap gap-3">
+        {traveler.childrenAges.map((age, indexAge) => (
+          <ChildrenAgesInput key={indexAge} age={age} indexAge={indexAge} />
         ))}
       </section>
     </section>
